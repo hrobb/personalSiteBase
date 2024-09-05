3fc0279ec0b087bb2d775ecb6898526643d0b56f
Hunter <hunter_robb@yahoo.com>
2024-09-04 20:57:31 -0500

    Small UI adjustments

--- app/ui/experience/summaryCard.tsx
+++ app/ui/experience/summaryCard.tsx
@@ -7,7 +7,7 @@ export default function SummaryCard({
 }) {
 
 	return (
-		<div className='flex flex-col max-w-full mx-auto max-w-3xl justify-center items-center transition transition-transform hover:scale-105 bg-white border-4 border-gray-500 p-6 m-6 rounded-lg drop-shadow cursor-pointer'>
+		<div className='flex flex-col mx-auto sm:max-w-5xl justify-center items-center transition transition-transform hover:scale-105 bg-white border-4 border-gray-500 p-6 m-6 rounded-lg drop-shadow cursor-pointer'>
 			<div className="p-4">
 				<h1 className='text-3xl font-bold'>{title}</h1>
 				<p>{text}</p>
